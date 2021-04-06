27dcf50bcc82b4c116cc002e598876710b2bd868
whytheplatypus <whytheplatypus@gmail.com>
2021-04-06 10:28:25 -0400

    Secure html generation in templatetag

--- regulations/templatetags/search.py
+++ regulations/templatetags/search.py
@@ -2,19 +2,26 @@ import re
 from django import template
 from django.template.defaultfilters import stringfilter
 from django.utils.safestring import mark_safe
+from django.utils.html import conditional_escape
 
 register = template.Library()
 
 el = ('<span class="search-highlight">', '</span>')
 
 
-@register.filter(name='highlight')
+@register.filter(name='highlight', needs_autoescape=True, is_safe=True)
 @stringfilter
-def highlight(text, arg):
+def highlight(text, arg,  autoescape=True):
+    if autoescape:
+        text = conditional_escape(text)
     search_terms = arg.split()
     for term in search_terms:
         text = wrap(term, el, text)
-    return mark_safe(text)
+    # Used to highlight search results originating
+    # from internal data sources, low risk of XSS.
+    # Also escapes the input first, only html being
+    # returned is generated within this function.
+    return mark_safe(text)  # nosec
 
 
 def wrap(term, el, text):
