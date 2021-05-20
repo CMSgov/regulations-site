53e090dbecf3fd87efce499827b91f4ee4bd0ef6
David Gage <whytheplatypus@gmail.com>
2021-05-20 13:30:06 -0400

    Styled related rules component

--- regulations/static/components/RelatedRule.vue
+++ regulations/static/components/RelatedRule.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="related-rule">
     <div class="related-rule-header">
-      <span class="related-rule-type">{{ expandedType }}</span>
-      <span class="related-rule-date">{{ effective_on|formatDate }}</span> |
-      <span class="related-rule-citation">{{ citation }}</span>
+      <span class="related-rule-type ds-c-badge ds-c-badge--final">{{ expandedType }}</span>
+      <span class="related-rule-date">{{ effective_on|formatDate }}</span><span class="related-rule-citation">{{ citation }}</span>
     </div>
     <div>
-      <a class="related-rule-title" :href="html_url">{{ title }}</a>
+      <a class="related-rule-title" :href="html_url">{{ title }} <i class="fas fa-external-link-alt"></i></a>
     </div>
   </div>
 </template>
