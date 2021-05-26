a4afa20eb197ff7271d3cb5964560adfd6ae533f
whytheplatypus <whytheplatypus@gmail.com>
2021-05-26 10:39:35 -0400

    Organize node_type templates in a sub dir

--- regulations/templatetags/paragraphs.py
+++ regulations/templatetags/paragraphs.py
@@ -5,7 +5,7 @@ section_depth = 2
 
 
 @register.filter(name='pdepth')
-def internalcitation(value):
+def pdepth(value):
     depth = len(value.get("label", []) or []) - section_depth
     if len(value.get("marker", []) or []) > 1:
         depth = depth - len(value.get("marker", []) or [])
--- regulations/templatetags/render_nested.py
+++ regulations/templatetags/render_nested.py
@@ -6,3 +6,10 @@ register = Library()
 @register.simple_tag()
 def render_nested(*templates, context=None, **kwargs):
     return loader.select_template(templates).render(context or kwargs)
+
+@register.filter
+def interpolate(value, arg):
+    try:
+        return value.format(**arg)
+    except:
+        return value
