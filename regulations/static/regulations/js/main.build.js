c588636212c8aa2390169bd3c26b9682caafe12c
Dawn L. Low <dawn@inkandsound.com>
2021-06-17 11:53:31 -0500

    Moved sticky header JS to main.js

--- regulations/static/regulations/js/main.build.js
+++ regulations/static/regulations/js/main.build.js
@@ -904,17 +904,17 @@
 
   // Sticky header
 
-  window.onscroll = function() {stickyHeader()};
+  window.onscroll = function() {stickyHeader();};
 
   var header = document.getElementById("header");
   var sticky = header.offsetTop;
 
   function stickyHeader() {
-    if (window.pageYOffset > sticky) {
-      header.classList.add("sticky");
-    } else {
-      header.classList.remove("sticky");
-    }
-  } 
+      if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+      } else {
+        header.classList.remove("sticky");
+      }
+  }
 
 }());
